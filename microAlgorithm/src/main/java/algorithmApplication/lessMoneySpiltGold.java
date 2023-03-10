3db8471ad5e4f4764abb99cbfbe0d8f2867125f1
liwang823 <844523449@qq.com>
2023-03-10 21:19:16 +0800

    sortTopology

--- microAlgorithm/src/main/java/algorithmApplication/lessMoneySpiltGold.java
+++ microAlgorithm/src/main/java/algorithmApplication/lessMoneySpiltGold.java
@@ -1,6 +1,28 @@
-package algorithmApplication;/**
+package algorithmApplication;
+
+import java.util.PriorityQueue;
+
+/**
  * @author 李旺
- * @date 2023/3/9 23:46
  * @version 1.0
- */public class lessMoneySpiltGold {
+ * @date 2023/3/9 23:46
+ */
+public class lessMoneySpiltGold {
+
+    public static int solution(int[] arr){
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int num : arr){
+            priorityQueue.add(num);
+        }
+
+        int sum = 0;
+        while (priorityQueue.size() > 1){
+            int temp = priorityQueue.poll() + priorityQueue.poll();
+            sum += temp;
+            priorityQueue.add(temp);
+        }
+
+        return sum;
+    }
 }
