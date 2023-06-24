d130ead5f5df138333ce20e5a71d8ef5afc9b2d3
liwang823 <844523449@qq.com>
2023-06-24 23:06:34 +0800

    demo

--- microAlgorithm/javaGrammarLearning/src/main/java/Demo02.java
+++ microAlgorithm/javaGrammarLearning/src/main/java/Demo02.java
@@ -1,6 +1,34 @@
-package PACKAGE_NAME;/**
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
  * @author 李旺
- * @date 2023/6/11 12:43
  * @version 1.0
- */public class Demo02 {
+ * @date 2023/6/11 12:43
+ */
+public class Demo02 {
+
+    public static void main(String[] args) {
+
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault(), Locale.CHINESE);
+
+        Date now = calendar.getTime();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+
+        System.out.println(simpleDateFormat.format(now));
+
+        String string = "2023年06月12日 23:34:00";
+
+        try {
+            System.out.println(simpleDateFormat.format(simpleDateFormat.parse(string)));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
