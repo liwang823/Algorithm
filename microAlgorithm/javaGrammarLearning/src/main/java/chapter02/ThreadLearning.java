89ff30edb67a43945d400372e5465b2bf31f4224
liwang823 <844523449@qq.com>
2023-09-27 21:13:36 +0800

    1.fix目录 2.序列化与反序列化 3.测试

--- microAlgorithm/javaGrammarLearning/src/main/java/chapter02/ThreadLearning.java
+++ microAlgorithm/javaGrammarLearning/src/main/java/chapter02/ThreadLearning.java
@@ -4,11 +4,109 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 并发知识的学习
  * @author 李旺
  * @version 1.0
- * @date 2023/9/23 23:15
+ * @date 2023/9/23 23:15230821
  */
 public class ThreadLearning {
+
+    static class GetAJ{
+
+        public synchronized void success() throws InterruptedException {
+
+            for (int i = 0; i < 10; i++){
+                System.out.println(Thread.currentThread().getName() + "success" + Thread.currentThread().getState());
+                Thread.sleep(2000);
+            }
+        }
+    }
+
+    static class UserBuyingProcess extends Thread{
+
+        private final GetAJ getAJ;
+        private final Thread nextThread;
+
+        public UserBuyingProcess(GetAJ getAJ, Thread nextThread){
+            this.getAJ = getAJ;
+            this.nextThread = nextThread;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                getAJ.success();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static void main(String[] args) throws InterruptedException {
+
+            // 买鞋主程序
+            int userNum = 10;
+            GetAJ getAJ = new GetAJ();
+            Thread[] users = new Thread[userNum];
+
+            for (int i = 0; i < users.length - 1; i++){
+                users[i] = new UserBuyingProcess(getAJ, users[i + 1]);
+                System.out.println("开始执行线程" + users[i].getName());
+                users[i].start();
+                System.out.println("----------------------------split----------------------------");
+            }
+
+            for (Thread user: users){
+                user.join();
+            }
+        }
+    }
+
+    /**
+     * 计数器在计数方法上面加了锁
+     */
+    static class Counter{
+        private int count;
+        public synchronized void incr(){
+            count++;
+        }
+
+        public synchronized int getCount(){
+            return count;
+        }
+    }
+
+    /**
+     * 计数功能的主程序，根据传入的计数器实例化
+     */
+    static class CounterThread extends Thread{
+        Counter counter;
+        public CounterThread(Counter counter){
+            this.counter = counter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++){
+                counter.incr();
+            }
+        }
+
+        public static void main(String[] args) throws InterruptedException {
+            int num = 1000;
+            Counter counter = new Counter();
+            Thread[] threads = new Thread[num];
+            for (int i = 0; i < threads.length; i++){
+                threads[i] = new CounterThread(counter);
+                threads[i].start();
+            }
+
+            for (Thread thread : threads) {
+                thread.join();
+            }
+            System.out.println(counter.getCount());
+        }
+    }
 
     static class HelloRunnable implements Runnable{
 
