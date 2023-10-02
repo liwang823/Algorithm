2d9a18fe548ad2defdec0a736addf81bc4ec705f
liwang823 <844523449@qq.com>
2023-10-02 22:24:09 +0800

    线程协作

--- microAlgorithm/javaGrammarLearning/src/main/java/chapter03/MultiThread.java
+++ microAlgorithm/javaGrammarLearning/src/main/java/chapter03/MultiThread.java
@@ -1,6 +1,41 @@
-package chapter03;/**
+package chapter03;
+
+import java.util.Map;
+
+/**
+ * 线程协作
  * @author 李旺
- * @date 2023/9/30 10:56
  * @version 1.0
- */public class multiThread {
+ * @date 2023/9/30 10:56
+ */
+public class MultiThread extends Thread{
+
+    private volatile boolean fire = false;
+
+    @Override
+    public void run() {
+        try {
+            synchronized (this){
+                while (! fire){
+                    wait();
+                }
+            }
+            System.out.println("fired");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void fire(){
+        this.fire = true;
+        notify();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MultiThread multiThread = new MultiThread();
+        multiThread.start();
+        Thread.sleep(1000);
+        System.out.println("fire");
+        multiThread.fire();
+    }
 }
